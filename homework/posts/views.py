502a2e32ee325003cdcfd22777bd2bfd8a19f9e1
OVERDOZZZE <saparbekov113@gmail.com>
2023-01-06 20:21:21 +0600

    Seventh homework

--- homework/posts/views.py
+++ homework/posts/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from datetime import datetime
 from .models import Product, Category, Reviews
 from .forms import ProductCreateForm, CommentCreateForm
+from .constants import PAGINATION_LIMIT
 # Create your views here.
 
 
@@ -11,18 +12,33 @@ def main_view(request):
 
 
 def posts_view(request):
-    if request.method =='GET':
+    if request.method == 'GET':
         category_id = int(request.GET.get('category_id', 0))
+        search = request.GET.get('search')
+        page = int(request.GET.get('page', 1))
+
         if category_id:
             posts = Product.objects.filter(category__in=[category_id])
         else:
             posts = Product.objects.all()
 
-    context = {
-        'posts': posts,
-        'user': None if request.user.is_anonymous else request.user
-    }
-    return render(request, 'posts/posts.html', context=context)
+        max_page = posts.__len__() / PAGINATION_LIMIT
+
+        if round(max_page) < max_page:
+            max_page = round(max_page) + 1
+
+        max_page = int(max_page)
+        posts = posts[PAGINATION_LIMIT * (page-1):PAGINATION_LIMIT * page]
+
+        if search:
+            posts = posts.filter(title__icontains=search)
+
+        context = {
+            'posts': posts,
+            'user': None if request.user.is_anonymous else request.user,
+            'max_page': range(1, max_page + 1)
+        }
+        return render(request, 'posts/posts.html', context=context)
 
 
 def post_detail_view(request, id):
